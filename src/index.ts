fee4e46647a6b8ac0241cb66e15a1c40b267b853
clbrunet <brunet.clement5@gmail.com>
2023-01-29 16:15:42 +0100

    feat: add basic GraphQL API using express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello world!");
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+type Query {
+  hello: String
+}
+`);
+
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+const app = express();
+const port = 3000;
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
